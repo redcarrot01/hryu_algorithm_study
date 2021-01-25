f92708a47c660d01b2afb8d96afea669ac10e836
redcarrot01 <redccc9010@gmail.com>
2021-01-25 13:18:18 +0900

    개미전사

---
+++ hryu_algorithm_study/hryu_algorithm_study/part2_dp_개미전사.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int d[100];
+int n;
+vector<int> arr;
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		arr.push_back(x);
+	}
+
+	// º¸ÅÒ ¾÷
+	d[0] = arr[0];
+	d[1] = max(arr[0], arr[1]);
+	for (int i = 2; i < n; i++) {
+		d[i] = max(d[i - 1], d[i - 2] + arr[i]);
+	}
+	cout << d[n - 1] << '\n';
+	return 0;
+}
