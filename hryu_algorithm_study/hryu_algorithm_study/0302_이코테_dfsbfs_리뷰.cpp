4cd51590a4358aacf71f774f1b8fe4d3350534c3
redcarrot01 <redccc9010@gmail.com>
2021-03-03 16:14:53 +0900

    연산자끼워넣기- 백트래킹 이해 잘 못한듯.. 탐색 어렵고 재귀 어렵다 이거 다시 풀기

--- hryu_algorithm_study/hryu_algorithm_study/0302_이코테_dfsbfs_리뷰.cpp
+++ hryu_algorithm_study/hryu_algorithm_study/0302_이코테_dfsbfs_리뷰.cpp
@@ -147,3 +147,209 @@ bfs
 //
 //    bfs(1);
 //}
+
+// 음료수 얼려먹기
+
+/*
+n*m 구멍뚫려있는 0, 칸막이 1
+n,m 1~1000
+한번에 만들수있는 아이스크림개수 
+00110
+00011
+11111
+00000   -> 3개 가능
+
+팁
+1. 상하좌우 살피기 -> 0이면서 방문하지 않았다면 방문
+2. 방문지점에서 과정 반복
+3. 방문안 한 지점 수 카운트
+*/
+//#include <stdio.h>
+//#include <iostream>
+//using namespace std;
+//
+//int n, m;
+//int graph[1001][1001];
+//
+//// dfs로 특정 노드를 방문하고 연결된 모든 노드들도 방문
+//bool dfs(int x, int y) {
+//	// 주어진 범위 벗어나는 경우에는 즉시 종료
+//	if (x < 0 || x >= n || y < 0 || y >= m) {
+//		return false;
+//	}
+//	// 현재 노드를 아직 방문하지 않는다면
+//	if (graph[x][y] == 0) {
+//		// 방문 처리
+//		graph[x][y] = 1;
+//		// 상하좌우
+//		dfs(x - 1, y);
+//		dfs(x, y - 1);
+//		dfs(x + 1, y);
+//		dfs(x, y + 1);
+//		return true;
+//	}
+//	return false;
+//}
+//
+//int main() {
+//	cin >> n >> m;
+//
+//	// 2차원 리스트의 맵 정보 입력, 
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j <m ; j++) {
+//			scanf_s("%1d", &graph[i][j]);
+//			// 위치매개변수 1은 다닥다닥 붙어있는 입력처리 위함
+//		}
+//	}
+//	// 모든 노드에 대하여 음료수 채우기
+//	int result = 0;
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < m; j++) {
+//			// 현재 위치에서 dfs 수행
+//			if (dfs(i, j)) result += 1;
+//		}
+//	}
+//	cout << result << '\n';
+//}
+
+
+
+
+// 미로탈출
+/*
+(1,1) -> (N,M)으로 도착
+괴물o 0, 괴물x 1
+탈출하기 위해 움직여야 하는 최소한의 개수
+
+n,m 1~200
+수들은 공백없이 주어짐
+
+팁 
+bfs => 시작노드에서 가까운노드부터 차례대로 그래프의 모든노드 탐색
+*/
+//# include <stdio.h>
+//# include <iostream>
+//# include <queue>
+//
+//using namespace std;
+//
+//int n, m;
+//int graph[201][201];
+//
+//// 방향 상하좌우
+//
+//int dx[] = { -1, 1, 0, 0 };
+//int dy[] = { 0, 0, -1, 1 };
+//
+//int bfs(int x, int y) {
+//	queue<pair<int, int>> q;
+//	q.push({ x,y });
+//
+//	while (!q.empty()) {
+//		int x = q.front().first; // (1,2) -> 1
+//		int y = q.front().second; // (1,2) -> 2
+//		q.pop();
+//		// 현재 위치에서 4가지 방향으로의 위치
+//		for (int i = 0; i < 4; i++) {
+//			int nx = x + dx[i];
+//			int ny = y + dy[i];
+//			if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+//				continue;
+//			if (graph[nx][ny] == 0)
+//				continue;
+//
+//			//해당 노드를 처음 방문하는 경우 최단 거리 기록
+//			if (graph[nx][ny] == 1) {
+//				graph[nx][ny] = graph[x][y] + 1;
+//				q.push({ nx, ny });
+//			}
+//		}
+//	}
+//	// 가장 오른쪽 아래까지의 최단 거리 방문
+//	return graph[n - 1][m - 1];
+//}
+//
+//int main() {
+//	cin >> n >> m;
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < m; j++) {
+//			scanf_s("%1d", &graph[i][j]);
+//		}
+//	}
+//	cout << bfs(0, 0) << '\n';
+//	return 0;
+//}
+
+// 연산자 끼워넣기
+/*
+n 2~11
+a 1~100
++ - * /  총 n-1개의 정수
+
+출력 : 최댓값, 최솟값 -10억 ~10억
+
+// 연산자는 operators라는 4짜리 배열 선언
+그 후, 백트래킹 사용하여 재귀 해결
+매개변수는 여태까지 연산의 result, 연산을 진행할 수의 인덱스 입력받도록 하는 
+백트래킹 재귀 함수 정의하여 풀이
+
+재귀의 흐름과 매개변수 어떤 값을 사용할지 설계하는 능력 필요
+
+2
+5 6 
+0 0 1 0    ->  30 30
+
+3
+3 4 5
+1 0 1 0  ->	 35 17
+
+*/
+
+# include <iostream>
+using namespace std;
+
+int N; // n 2~11
+int operands[11];
+int operators[4];
+int mymin = 1000000001;
+int mymax = -1000000001;
+
+// 재귀로
+void get_answer(int result, int idx) {
+	if (idx == N) { // 연산자 다 사용
+		if (result > mymax) mymax = result;
+		if (result < mymin) mymin = result;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		if (operators[i] > 0) { // 그 인덱스에 연산자 살아있다면
+			operators[i]--; // 연산자 사용하면 한개 줄여줘
+			if (i == 0)
+				get_answer(result + operands[idx], idx + 1);
+			else if (i == 1)
+				get_answer(result - operands[idx], idx + 1);
+			else if (i == 2)
+				get_answer(result * operands[idx], idx + 1);
+			else
+				get_answer(result / operands[idx], idx + 1);
+
+			operators[i]++; // 다른 연산자 사용할 것이므로 
+			// 아까 줄였던 연산자 개수 늘려줌
+		}
+	}
+	return;
+}
+
+int main() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> operands[i];
+	}
+	for (int i = 0; i < 4; i++) {
+		cin >> operators[i];
+	}
+	get_answer(operands[0], 1); // idx는 연산자 개수, 1개부터 시작이니까
+	cout << mymax << '\n';
+	cout << mymin << '\n';
+
+}
